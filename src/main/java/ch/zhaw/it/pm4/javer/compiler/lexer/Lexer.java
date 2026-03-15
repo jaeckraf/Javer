1f14788fff48f12e04214b8a5dcaef5f3a16d7aa
Stefan Paul <paulste1@students.zhaw.ch>
2026-03-15 19:52:14 +0100

    structure

---
+++ src/main/java/ch/zhaw/it/pm4/javer/compiler/lexer/Lexer.java
@@ -0,0 +1,4 @@
+package ch.zhaw.it.pm4.javer.compiler.lexer;
+
+public class Lexer {
+}
---
+++ src/main/java/ch/zhaw/it/pm4/javer/compiler/parser/parsenode/ParserNode.java
@@ -0,0 +1,4 @@
+package ch.zhaw.it.pm4.javer.compiler.parser.parsenode;
+
+public abstract class ParserNode {
+}
