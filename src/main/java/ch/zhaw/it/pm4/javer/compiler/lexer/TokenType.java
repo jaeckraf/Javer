125cd0b5c788dcbd087f627c90457b4b032c84a8
Stefan Paul <paulste1@students.zhaw.ch>
2026-03-23 19:23:26 +0100

    Made packages according to structure.

--- src/main/java/ch/zhaw/it/pm4/javer/compiler/lexer/TokenType.java
+++
@@ -1,4 +0,0 @@
-package ch.zhaw.it.pm4.javer.compiler.lexer;
-
-public enum TokenType {
-}
--- src/main/java/ch/zhaw/it/pm4/javer/compiler/parser/visitor/ParseTreeBaseVisitor.java
+++
@@ -1,4 +0,0 @@
-package ch.zhaw.it.pm4.javer.compiler.parser.visitor;
-
-public class ParseTreeBaseVisitor {
-}
