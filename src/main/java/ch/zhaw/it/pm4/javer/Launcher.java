437301b4654bacd1d911672c69eed23aebfdc2f8
Eric Gahlinger (Gahlieri) <gahlieri@students.zhaw.ch>
2026-04-07 18:07:05 +0200

    SCRUM-50: User can open the program. Created branch

--- src/main/java/ch/zhaw/it/pm4/javer/Launcher.java
+++ src/main/java/ch/zhaw/it/pm4/javer/Launcher.java
@@ -3,6 +3,7 @@ package ch.zhaw.it.pm4.javer;
 import ch.zhaw.it.pm4.javer.gui.GuiApplication;
 import javafx.application.Application;
 
+// Main Launcher to start Javer
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(GuiApplication.class, args);
