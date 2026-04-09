7a8fdb59b4b353a8a19b65120308333a8da518a0
Eric Gahlinger (Gahlieri) <gahlieri@students.zhaw.ch>
2026-04-09 11:31:16 +0200

    SCRUM-50: User can open the program. Fixed error

--- src/main/java/ch/zhaw/it/pm4/javer/vm/VM.java
+++ src/main/java/ch/zhaw/it/pm4/javer/vm/VM.java
@@ -2,11 +2,9 @@ package ch.zhaw.it.pm4.javer.vm;
 
 public class VM {
     private BytecodeLoader bytecodeLoader;
-    private BytecodeModule bytecodeModule;
     private VMState vmState = new VMState();
 
-    public VM(BytecodeLoader bytecodeLoader, BytecodeModule bytecodeModule) {
+    public VM(BytecodeLoader bytecodeLoader) {
         this.bytecodeLoader = bytecodeLoader;
-        this.bytecodeModule = bytecodeModule;
     }
 }
