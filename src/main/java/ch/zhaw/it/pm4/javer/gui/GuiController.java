a4ceb1fb7b0f35f4373d6eb9ced7402cc561a46e
Surajeev <sanjesur@students.zhaw.ch>
2026-04-10 14:38:25 +0200

    updated UI

--- src/main/java/ch/zhaw/it/pm4/javer/gui/GuiController.java
+++ src/main/java/ch/zhaw/it/pm4/javer/gui/GuiController.java
@@ -1,21 +1,37 @@
 package ch.zhaw.it.pm4.javer.gui;
 
+
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class GuiController {
+
     @FXML
-    private TextArea consoleOutput;
+    private TextArea compilerOutput;
+
+    @FXML
+    private TextArea virtualMachineOutput;
+
+    @FXML
+    private TextArea consoleInput;
 
     @FXML
     private Button runCompilerButton;
 
     @FXML
     protected void onRunCompilerClick() {
-        // Mock compiler output
-        consoleOutput.appendText("Starting compiler...\n");
-        consoleOutput.appendText("Mock compiling...\n");
-        consoleOutput.appendText("Compiler finished successfully.\n");
+        String input = consoleInput.getText();
+
+        compilerOutput.appendText("Starting compiler...\n");
+        if (input != null && !input.isBlank()) {
+            compilerOutput.appendText("Input: " + input + "\n");
+        }
+        compilerOutput.appendText("Mock compiling...\n");
+        compilerOutput.appendText("Compiler finished successfully.\n");
+
+        consoleInput.clear();
     }
 }
