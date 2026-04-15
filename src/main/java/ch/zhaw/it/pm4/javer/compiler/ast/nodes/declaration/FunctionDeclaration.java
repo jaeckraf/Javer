8c26eb30c3a621d3813af56533b652f5f089b1b4
Stefan <StefanpaulSchool@gmail.com>
2026-04-15 15:03:41 +0200

    fix function

--- src/main/java/ch/zhaw/it/pm4/javer/compiler/ast/nodes/declaration/FunctionDeclaration.java
+++ src/main/java/ch/zhaw/it/pm4/javer/compiler/ast/nodes/declaration/FunctionDeclaration.java
@@ -1,5 +1,6 @@
 package ch.zhaw.it.pm4.javer.compiler.ast.nodes.declaration;
 
+import ch.zhaw.it.pm4.javer.compiler.ast.nodes.statement.BlockStatement;
 import ch.zhaw.it.pm4.javer.compiler.ast.nodes.type.TypeAstNode;
 import ch.zhaw.it.pm4.javer.compiler.visitor.AstNodeVisitor;
 
@@ -11,6 +12,13 @@ public final class FunctionDeclaration implements DeclarationAstNode {
     private final String name;
     private final List<FunctionParameter> parameters;
     private final BlockStatement body;
+
+    public FunctionDeclaration(TypeAstNode returnType, String name, List<FunctionParameter> parameters, BlockStatement body) {
+        this.returnType = returnType;
+        this.name = name;
+        this.parameters = parameters;
+        this.body = body;
+    }
 
     @Override
     public <T> T accept(AstNodeVisitor<T> visitor) {
