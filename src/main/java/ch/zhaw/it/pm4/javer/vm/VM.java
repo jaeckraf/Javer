3408ce09aa22afebfa87ba9d041e12e123561bf2
Stefan Paul <paulste1@students.zhaw.ch>
2026-04-19 21:02:34 +0200

    WIP 1, Meeting 19.04.2026

--- src/main/java/ch/zhaw/it/pm4/javer/vm/VM.java
+++ src/main/java/ch/zhaw/it/pm4/javer/vm/VM.java
@@ -1,47 +1,60 @@
 package ch.zhaw.it.pm4.javer.vm;
 
-import ch.zhaw.it.pm4.javer.vm.memorySegments.DataSegment;
-import ch.zhaw.it.pm4.javer.vm.memorySegments.Heap;
-import ch.zhaw.it.pm4.javer.vm.memorySegments.MemorySegment;
-
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class VM {
 
-    private BytecodeLoader bytecodeLoader;
+    private String[] instructions;
+    private List<Integer> dataSegment;
+    private int stack[] = new int[4096];
+    private int stackPointer;
 
-    public VM(BytecodeLoader bytecodeLoader) {
-        this.bytecodeLoader = bytecodeLoader;
+    public VM(String filePath) {
+        File file = new File(filePath);
+        try {
+            String content = new String(Files.readAllBytes(Paths.get(filePath)));
+            instructions = content.split("\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String run() {
        int programCounter = 0;
        boolean isRunning = true;
-       MemorySegment staticSegment; // TODO sind im Moment Dummies (MemorySegment)
-       MemorySegment argumentSegment; // TODO sind im Moment Dummies (MemorySegment)
-       MemorySegment localSegment; // TODO sind im Moment Dummies (MemorySegment)
-       MemorySegment pointerSegment; // TODO sind im Moment Dummies (MemorySegment)
-       Heap heap;
 
        while(isRunning) {
-           Instruction instruction = bytecodeLoader.getInstructions().get(programCounter);
-           List<Operand> operands = instruction.getOperands();
-           switch (instruction.getOperationCode()) {
-               case OPCode.HALT -> {
-                   isRunning = false;
-                   // TODO implement add
-               }
-               default -> {
-                     // TODO implement add
-                   isRunning = false;
-                   return new VMError("Unknown OPCode: " + instruction.getOperationCode(), programCounter, instruction).toString();
-               }
+           String instruction = instructions[programCounter];
+           String[] tokens = instruction.split(" ");
+           switch(tokens[0]) {
+               case "ADD": {
 
+
+               } break;
+               case "PUSH": {
+                   Double.parseDouble(tokens[1]);
+                   tokens[1].charAt(1);
+               } break;
            }
 
-       }
-        return "Successfully executed program";
 
+           programCounter++;
+       }
+
+
+        return "";
+    }
+
+    private void processCodeAndDataSegments() {
+
+    }
+
+    private void push(int value) {
+        stack[stackPointer] = value;
     }
 
 }
