33c67690ceec2b3a0188185f0853e3c57e690b2c
Stefan <StefanpaulSchool@gmail.com>
2026-04-22 17:30:19 +0200

    wip

--- VM/src/main/java/ch/zhaw/it/pm4/javer/vm/VM.java
+++ VM/src/main/java/ch/zhaw/it/pm4/javer/vm/VM.java
@@ -1,60 +1,38 @@
 package ch.zhaw.it.pm4.javer.vm;
 
-import ch.zhaw.it.pm4.javer.vm.memorySegments.Heap;
-import ch.zhaw.it.pm4.javer.vm.memorySegments.MemorySegment;
 
-import java.util.List;
+/*
+* code:
+*
+*
+*
+* data:
+*
+* */
+
+
 
 public class VM {
 
-    private BytecodeLoader bytecodeLoader;
 
-    public VM(BytecodeLoader bytecodeLoader) {
-        this.bytecodeLoader = bytecodeLoader;
-    }
 
     public static void main(String[] args) {
-        System.setProperty("MODULE", "vm");
-        if (args.length < 1) {
-            System.err.println("Usage: java VM <bytecode-file>");
-            System.exit(1);
+        if (args.length == 0) {
+            System.out.println("Usage: java VM <filePath>");
+            return;
         }
-        String bytecodeFile = args[0];
-        BytecodeLoader bytecodeLoader = new BytecodeLoader(bytecodeFile);
-        VM vm = new VM(bytecodeLoader);
-        String result = vm.run();
-        System.out.println("test");
-        System.err.println("error test");
+        String filePath = args[0];
+        VM vm = new VM(filePath);
+        vm.run();
     }
 
-    public String run() {
-       int programCounter = 0;
-       boolean isRunning = true;
-       MemorySegment staticSegment; // TODO sind im Moment Dummies (MemorySegment)
-       MemorySegment argumentSegment; // TODO sind im Moment Dummies (MemorySegment)
-       MemorySegment localSegment; // TODO sind im Moment Dummies (MemorySegment)
-       MemorySegment pointerSegment; // TODO sind im Moment Dummies (MemorySegment)
-       Heap heap;
-
-       while(isRunning) {
-           Instruction instruction = bytecodeLoader.getInstructions().get(programCounter);
-           List<Operand> operands = instruction.getOperands();
-           switch (instruction.getOperationCode()) {
-               case OPCode.HALT -> {
-                   isRunning = false;
-                   // TODO implement add
-               }
-               default -> {
-                     // TODO implement add
-                   isRunning = false;
-                   return new VMError("Unknown OPCode: " + instruction.getOperationCode(), programCounter, instruction).toString();
-               }
-
-           }
-
-       }
-        return "Successfully executed program";
+    public VM(String filePath) {
 
     }
+
+    public void run() {
+
+    }
+
 
 }
