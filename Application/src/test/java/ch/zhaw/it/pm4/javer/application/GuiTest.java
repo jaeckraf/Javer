4464548714745166415b8894e75f2aa0a4962371
Stefan Paul <paulste1@students.zhaw.ch>
2026-05-15 16:50:58 +0200

    fix gui test, fxml loader

--- Application/src/test/java/ch/zhaw/it/pm4/javer/application/GuiTest.java
+++ Application/src/test/java/ch/zhaw/it/pm4/javer/application/GuiTest.java
@@ -5,24 +5,35 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.testfx.api.FxAssert;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
-import org.testfx.matcher.base.NodeMatchers;
 
 import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(ApplicationExtension.class)
 class GuiTest {
 
-    @Test
-    void shouldRenderAllMainGuiControls() {
-        FxAssert.verifyThat("#consoleInput", NodeMatchers.isNotNull());
-        FxAssert.verifyThat("#runCompilerButton", NodeMatchers.isNotNull());
-        FxAssert.verifyThat("#compilerOutput", NodeMatchers.isNotNull());
-        FxAssert.verifyThat("#virtualMachineOutput", NodeMatchers.isNotNull());
-        FxAssert.verifyThat("#vmStackSizeOption", NodeMatchers.isNotNull());
-        FxAssert.verifyThat("#vmDumpOnErrorOption", NodeMatchers.isNotNull());
+    private Map<String, Object> namespace;
+
+    @Start
+    void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GuiApplication.class.getResource("gui-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        namespace = fxmlLoader.getNamespace();
+        stage.setScene(scene);
+        stage.show();
     }
 
+    @Test
+    void shouldLoadMainGuiControlsFromFxml() {
+        assertNotNull(namespace.get("consoleInput"));
+        assertNotNull(namespace.get("runCompilerButton"));
+        assertNotNull(namespace.get("compilerOutput"));
+        assertNotNull(namespace.get("virtualMachineOutput"));
+        assertNotNull(namespace.get("vmStackSizeOption"));
+        assertNotNull(namespace.get("vmDumpOnErrorOption"));
+    }
 }
