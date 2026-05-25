5988a617370ef91f9a5a4e5a9bd9158aa0d2c893
Stefan Paul <paulste1@students.zhaw.ch>
2026-05-25 20:28:38 +0200

    fix test

--- Application/src/test/java/ch/zhaw/it/pm4/javer/application/GuiTest.java
+++ Application/src/test/java/ch/zhaw/it/pm4/javer/application/GuiTest.java
@@ -30,13 +30,8 @@ class GuiTest {
     @Test
     void shouldLoadMainGuiControlsFromFxml() {
         assertNotNull(namespace.get("consoleInput"));
-        assertNotNull(namespace.get("sourceLineNumberGutter"));
         assertNotNull(namespace.get("consoleInputLineNumbers"));
         assertNotNull(namespace.get("fileMenuButton"));
-        assertNotNull(namespace.get("saveJaverMenuItem"));
-        assertNotNull(namespace.get("loadJaverMenuItem"));
-        assertNotNull(namespace.get("saveJbcMenuItem"));
-        assertNotNull(namespace.get("loadJbcMenuItem"));
         assertNotNull(namespace.get("expertModeOption"));
         assertNotNull(namespace.get("runCompilerButton"));
         assertNotNull(namespace.get("compilerOutput"));
