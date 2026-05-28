ec25c110554f672c95cf6f50087ece2d8d2e8250
jaeckraf <jaeckraf@students.zhaw.ch>
2026-05-29 00:16:17 +0200

    removed asserts requested in sonarqube
    This reverts commit 0e0e31c3ffca0a4e68e28df05a10bc2143a6b8c0

--- Application/src/test/java/ch/zhaw/it/pm4/javer/application/GuiTest.java
+++ Application/src/test/java/ch/zhaw/it/pm4/javer/application/GuiTest.java
@@ -388,10 +388,6 @@ class GuiTest {
         TextArea statusOutput = robot.lookup("#statusOutput").queryAs(TextArea.class);
         robot.interact(() -> ((CheckMenuItem) namespace.get("expertModeOption")).setSelected(true));
         waitFor(2, TimeUnit.SECONDS, () -> statusOutput.getText().contains("Expert Mode set to On."));
-        assertTrue(
-                statusOutput.getText().contains("Expert Mode set to On."),
-                "Status output should confirm expert mode toggle"
-        );
     }
 
     @Test
@@ -491,10 +487,6 @@ class GuiTest {
 
             TextArea statusOutput = robot.lookup("#statusOutput").queryAs(TextArea.class);
             waitFor(5, TimeUnit.SECONDS, () -> statusOutput.getText().contains("Configured jar does not exist"));
-            assertTrue(
-                    statusOutput.getText().contains("Configured jar does not exist"),
-                    "Expected error message about missing compiler jar"
-            );
         } finally {
             System.setProperty("javer.compiler.jar", originalCompilerJar);
         }
@@ -540,17 +532,9 @@ class GuiTest {
 
         TextArea statusOutput = robot.lookup("#statusOutput").queryAs(TextArea.class);
         waitFor(5, TimeUnit.SECONDS, () -> statusOutput.getText().contains("Dump Lexer set to On."));
-        assertTrue(
-                statusOutput.getText().contains("Dump Lexer set to On."),
-                "Expected log message for enabling Dump Lexer"
-        );
 
         robot.clickOn("#compilerDumpLexerOption");
         waitFor(5, TimeUnit.SECONDS, () -> statusOutput.getText().contains("Dump Lexer set to Off."));
-        assertTrue(
-                statusOutput.getText().contains("Dump Lexer set to Off."),
-                "Expected log message for disabling Dump Lexer"
-        );
     }
 
     @Test
@@ -594,10 +578,6 @@ class GuiTest {
 
             TextArea statusOutput = robot.lookup("#statusOutput").queryAs(TextArea.class);
             waitFor(5, TimeUnit.SECONDS, () -> statusOutput.getText().contains("Configured jar does not exist"));
-            assertTrue(
-                    statusOutput.getText().contains("Configured jar does not exist"),
-                    "Expected VM error message when VM jar is missing"
-            );
         } finally {
             System.setProperty("javer.vm.jar", originalVmJar);
         }
